62faf7dbcedb0f7516594cefa68b53649112fccb
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-08-28 23:24:18 -0400

    initial commit

---
+++ problem_2.js
@@ -0,0 +1,20 @@
+const evenFibonacciNum = () => {
+  let a = 0
+  let b = 1
+  let c = 0
+  let sum = c
+
+  while (c <= 4000000) {
+    c = a + b
+    a = b
+    b = c
+    if (c % 2 === 0) {
+      sum += c
+    }
+  }
+
+  return sum 
+
+}
+
+console.log(evenFibonacciNum(33))
