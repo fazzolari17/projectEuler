5181ec385ff17f85185ceadc3d227b16a0c99b07
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2023-01-11 16:43:24 -0500

    add solutions for problems 5, 6, 7

--- problem_6.js
+++ problem_6.js
@@ -1,22 +1,41 @@
-const theSumSquareDifference = n => {
-  const sumOfTheSquares = num => {
-    let total = 0
-    for (let i = 1; i <= num; i++) {
-      total += i * i
-    }
-    return total
-  }
+// const theSumSquareDifference = n => {
+//   const sumOfTheSquares = num => {
+//     let total = 0
+//     for (let i = 1; i <= num; i++) {
+//       total += i * i
+//     }
+//     return total
+//   }
 
-  const squareOfTheSums = num => {
-    let total = 0
-    for (let i = 1; i <= num; i++) {
-      total += i
-    }
-    total = total * total
-    return total
-  }
+//   const squareOfTheSums = num => {
+//     let total = 0
+//     for (let i = 1; i <= num; i++) {
+//       total += i
+//     }
+//     total = total * total
+//     return total
+//   }
 
-  return squareOfTheSums(n) - sumOfTheSquares(n)
+//   return squareOfTheSums(n) - sumOfTheSquares(n)
+// }
+
+// console.log(theSumSquareDifference(100))
+
+
+
+const isPrime = num => {
+  for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+    if(num % i === 0) return false
+  return num > 1
 }
 
-console.log(theSumSquareDifference(100))
+const arrayOfNumbers = num => {
+  const array = []
+  for (let i = 0; i < num; i++) {
+    array.push(i)
+  }
+  return array
+}
+
+arrayOfNumbers(1000000)
+
