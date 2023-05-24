3f8c882aaa96a5d8076e67418aba28269c810e6c
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2023-05-23 21:35:15 -0400

    updated the solution to work on free code camp version

--- problem_5.js
+++ problem_5.js
@@ -1,18 +1,27 @@
 const start = Date.now()
 
-const smallestMultiple = () => {
-
-  let i = 20
-  while (i % 2 !== 0 || i % 3 !== 0 || i % 4 !== 0 || i % 5 !== 0 || i % 6 !== 0
-    || i % 7 !== 0 || i % 8 !== 0 || i % 9 !== 0 || i % 10 !== 0 || i % 11 !== 0
-    || i % 12 !== 0 || i % 13 !== 0 || i % 14 !== 0 || i % 15 !== 0 || i % 16 !== 0
-    || i % 17 !== 0 || i % 18 !== 0 || i % 19 !== 0 || i % 20 !== 0) {
-    i += 20
+function smallestMultiple(n) {
+  let largest = n
+  let unsolved = true
+  let i
+  while (unsolved) {
+    for (i = 2; i < n; i++) {
+      if (largest % i !== 0) {
+        largest = largest + n
+        break
+      }
+    }
+    if (i === n) {
+      unsolved = false
+      return largest
+    }
   }
-  return i
+  return largest
 }
+
+console.log(smallestMultiple(20))
 
 const end = Date.now()
 console.log(`${end - start}MS`)
 
-console.log(smallestMultiple())
+// console.log(smallestMult(20))
