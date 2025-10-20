5cd72fc3a533edffeab5319b9fd543bfbf0086cf
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2025-10-20 07:09:58 -0400

    added missing info  on readme

--- problem_52.py
+++ problem_52.py
@@ -1,3 +1,12 @@
+"""
+Permuted Multiples
+
+It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a different order.
+Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
+
+Link to the problem:
+    https://projecteuler.net/problem=52
+"""
 def find_multiples(n):
     multiples = []
     for i in range(2, 7):
